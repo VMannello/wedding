3984202cb3b77cb3ff61ef84aed7c363f30971a8
Vinny Mannello <vincent.mannello@gmail.com>
2017-03-21 23:36:27 -0700

    rsvp added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
+//Globals
+
+var $page2;
+var $page3;
+var $page4;
+var $menu;
+
 //Make it sticky
 $('.sticky').Stickyfill();
 
@@ -15,4 +22,92 @@ $(function() {
       }
     }
   });
+});
+
+//RSVP
+$(function() {
+
+
+  clicked = false;
+  
+  $('.rsvp').click(function(){
+        $page2 = $('#page2');
+        $page3 = $('#page3');
+        $page4 = $('#page4');
+        $menu = $('.menu');
+        $('#page2').remove();
+        $('#page3').remove();
+        $('#page4').remove();
+        $('.menu').remove();
+        $('.mv').slideUp();
+        $('.portland').animate({padding:'4vh 0 0 0'});
+        $('.mainbg').animate({height:'40vh'}, 1000);
+        //Add RSVP DOM
+        $('#rsvpContainer').show();
+
+      });
+
+      $('#cancelRSVP').click(function(){
+            $('#rsvpContainer').hide();
+            $('.mainbg').animate({height:'80vh'}, 1000);
+            $('.portland').animate({padding:'0 0 0 0'});
+            $('.mv').slideDown();
+            $('#page1').after($menu);
+            $('.menu').after($page2);
+            $('#page2').after($page3);
+            $('#page3').after($page4);
+            $('a[href*="#"]:not([href="#"])').click(function() {
+              if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                  $('html, body').animate({
+                    scrollTop: target.offset().top
+                  }, 400);
+                  return false;
+                }
+              }
+            });
+        });
+
+        $('#sendRSVP').click(function(){
+          wrong = 0;
+          $("#rsvpContainer > .required").each(function(){  
+            if( !$(this).val().length > 0) {
+                wrong ++;
+                $(this).css('box-shadow','0 0 3px #CC0000');
+            }
+          });
+          
+          if (wrong === 0) {
+            payload = {data:$('#rsvpContainer').serialize()};
+            console.log(payload);
+          }
+
+        });
+
+        $('#guestUp').click(function(){
+          $("#guestCount").text( parseInt($("#guestCount").text())+1 );
+          $("#attend").after('<input type="text" name="guest'+$("#guestCount").text()+'" class="rsvpText gone required" placeholder="Guests Name"></input>');
+        });
+
+        $('#guestDown').click(function(){
+          if (parseInt($("#guestCount").text()) > 0) {
+            $("input[name='guest"+$("#guestCount").text()+"']").remove();
+            $("#guestCount").text( parseInt($("#guestCount").text())-1 );
+          }
+          else {
+            $("#guestCount").text('0');
+          }
+        });
+
+        $('#notAttending').click(function(){
+          $('.gone').hide();
+          $('.gone').hide();
+        });
+        $('#attending').click(function(){
+          $('.gone').show();
+          $('.gone').show();
+        });
+
 });
