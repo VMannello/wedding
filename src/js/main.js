d14a02fa8061a47f629ad4a2affe671e424a2807
VMannello <git@vincentmannello.com>
2017-03-22 13:23:02 -0700

    Working on RSVP

--- src/js/main.js
+++ src/js/main.js
@@ -76,50 +76,54 @@ $(function() {
             wrong ++;
             $('input[type="radio"] + label').css('box-shadow','0 0 3px #CC0000');
           }
-            $("#rsvpContainer > .required").each(function(){  
-              if( !$(this).val().length > 0) {
+            $("#rsvpContainer > .required").each(function(){
+              if ( $(this).val().length !== 0 ) {
                   wrong ++;
                   $(this).css('box-shadow','0 0 3px #CC0000');
               }
             });
           if (wrong === 0) {
-            $('#rsvpContainer').html('<h2>RSVP Sent!!</h2>');
-            payload = {data:$('#rsvpContainer').serialize()};
-            console.log(payload);
-            $('.mainbg').animate({height:'75vh'}, 1000, 'swing', function() {
-              $('#rsvpContainer').hide();
+            payload = {message:$('#rsvpContainer').serialize()};
+            $.post('https://us-central1-themannellos-main.cloudfunctions.net/rsvp-V1', payload, function(d,s) {
+                $('#rsvpContainer').html('<h2>RSVP Sent!!</h2>');
+                $('.mainbg').animate({height:'75vh'}, 1000, 'swing', function() {
+                  $('#rsvpContainer').hide();
+                });
+                $('.portland').animate({padding:'0 0 0 0'});
+                $('.mv').slideDown();
+                $('#page1').after($menu);
+                $('.menu').after($page2);
+                $('#page2').after($page3);
+                $('#page3').after($page4);
+                $('a[href*="#"]:not([href="#"])').click(function() {
+                  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                    if (target.length) {
+                      $('html, body').animate({
+                        scrollTop: target.offset().top
+                      }, 400);
+                      return false;
+                    }
+                  }
+                });
+            } ).fail(function() {
+                $('#rsvpContainer').html('<h2>Error...</h2><h3>Try reloading the page and resubmitting.</h3>');
             });
-            $('.portland').animate({padding:'0 0 0 0'});
-            $('.mv').slideDown();
-            $('#page1').after($menu);
-            $('.menu').after($page2);
-            $('#page2').after($page3);
-            $('#page3').after($page4);
-            $('a[href*="#"]:not([href="#"])').click(function() {
-              if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                  $('html, body').animate({
-                    scrollTop: target.offset().top
-                  }, 400);
-                  return false;
-                }
-              }
-            });
+
           }
 
         });
 
         $('#guestUp').click(function(){
-          $("#guestCount").text( parseInt($("#guestCount").text())+1 );
+          $("#guestCount").text( parseInt($("#guestCount").text(), 10)+1 );
           $("#attend").after('<input type="text" name="guest'+$("#guestCount").text()+'" class="rsvpText gone required" placeholder="Guests Name"></input>');
         });
 
         $('#guestDown').click(function(){
-          if (parseInt($("#guestCount").text()) > 0) {
+          if (parseInt($("#guestCount").text(), 10) > 0) {
             $("input[name='guest"+$("#guestCount").text()+"']").remove();
-            $("#guestCount").text( parseInt($("#guestCount").text())-1 );
+            $("#guestCount").text( parseInt($("#guestCount").text(), 10)-1 );
           }
           else {
             $("#guestCount").text('0');
